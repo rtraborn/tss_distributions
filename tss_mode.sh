3bd9db0e3bf067be30b0ed930c8831d40bb4b7a7
rtraborn <rtraborn@asu.edu>
2019-06-28 17:50:45 -0400

    Updated the submission script.

--- tss_mode.sh
+++ tss_mode.sh
@@ -3,8 +3,7 @@
 #PBS -N hs_tss_mode
 #PBS -k o
 #PBS -l nodes=1:ppn=16,vmem=32gb
-#PBS -l walltime=1:00:00
-#PBS -q debug
+#PBS -l walltime=8:00:00
 
 myDir=/N/dc2/scratch/rtraborn/tss_distributions
 
