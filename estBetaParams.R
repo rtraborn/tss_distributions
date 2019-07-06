cb86c044ef498aa461ab39e5da2dc5170e816c66
R. Taylor Raborn <rtraborn@asu.edu>
2019-07-05 17:50:12 -0700

    Adding more plots and RData binary.

---
+++ estBetaParams.R
@@ -0,0 +1,6 @@
+#function written by user Max at StackExchange here: https://stats.stackexchange.com/a/12239
+estBetaParams <- function(mu, var) {
+  alpha <- ((1 - mu) / var - 1 / mu) * mu ^ 2
+  beta <- alpha * (1 / mu - 1)
+  return(params = list(alpha = alpha, beta = beta))
+}
